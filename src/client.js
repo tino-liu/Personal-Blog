8dc1cf93d8f33f2597e21fa8607ef6ef69f28808
Tino Liu <justinoliu.work@gmail.com>
2022-02-06 20:58:57 +0800

    Updated env configuration

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
-  dataset: process.env.SANITY_STUDIO_API_DATASET || "production",
-  projectId: process.env.SANITY_STUDIO_API_PROJECT_ID || "ovsx12m8",
+  dataset: process.env.REACT_APP_SANITY_STUDIO_API_DATASET,
+  projectId: process.env.REACT_APP_SANITY_STUDIO_API_PROJECT_ID,
   useCdn: true,
   apiVersion: "2021-08-05",
 });
