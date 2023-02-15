27b1229f874394c27c2eb6ff93ee4ff0f1eebc13
Tino Liu <justinoliu.work@gmail.com>
2023-02-15 11:28:00 +0800

    UI: changed npm package to yarn

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
-  dataset: process.env.REACT_APP_SANITY_STUDIO_API_DATASET,
-  projectId: process.env.REACT_APP_SANITY_STUDIO_API_PROJECT_ID,
+  dataset: "production",
+  projectId: "ovsx12m8",
   useCdn: true,
   apiVersion: "2021-08-05",
 });
