b90df07d669caba2fb528c727af03e7376ee5894
Tino Liu <justinoliu.work@gmail.com>
2023-02-15 11:29:48 +0800

    UI: Update client for specifying hosting or production

--- src/client.js
+++ src/client.js
@@ -1,8 +1,16 @@
 import sanityClient from "@sanity/client";
 
+// export default sanityClient({
+//   dataset: "production",
+//   projectId: "ovsx12m8",
+//   useCdn: true,
+//   apiVersion: "2021-08-05",
+// });
+
+// For Hosting
 export default sanityClient({
-  dataset: "production",
-  projectId: "ovsx12m8",
+  dataset: process.env.REACT_APP_SANITY_STUDIO_API_DATASET,
+  projectId: process.env.REACT_APP_SANITY_STUDIO_API_PROJECT_ID,
   useCdn: true,
   apiVersion: "2021-08-05",
 });
