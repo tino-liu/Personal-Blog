4756a81d65357ca883a1045e103054503b520859
Tino Liu [SSW] <tinoliu@ssw.com.au>
2024-03-24 12:11:29 +0800

    remove projectid

--- src/client.js
+++ src/client.js
@@ -1,12 +1,5 @@
 import sanityClient from "@sanity/client";
 
-// export default sanityClient({
-//   dataset: "production",
-//   projectId: "ovsx12m8",
-//   useCdn: true,
-//   apiVersion: "2021-08-05",
-// });
-
 // For Hosting
 export default sanityClient({
   dataset: process.env.REACT_APP_SANITY_STUDIO_API_DATASET,
